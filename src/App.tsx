46c5dd62930ffae6e5dc7375e17e3fd8bd95402e
Tarun <tarunb@udel.edu>
2024-09-05 10:32:42 -0400

    Updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,6 +8,48 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Tarun website</h1>
+            <img src="../public/tasks/dog.jpg" alt="dog" />
+            <div style={{ border: "1px solid blue", padding: "4px" }}>
+                <ul>
+                    <li>The Cat in the Hat</li>
+                    <li>Thing 1</li>
+                    <li>Thing 2</li>
+                </ul>
+            </div>
+            <div>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+            </div>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "72px",
+                                    height: "96px",
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "72px",
+                                    height: "96px",
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             <p>
                 Hello World Tarun Baskaran Edit <code>src/App.tsx</code> and
                 save. This page will automatically reload.
